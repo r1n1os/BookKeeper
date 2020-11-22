6ec65540294c38f9dc0c71f118c5722f982c60db
evagoras <r1n1os.dev@gmail.com>
2020-11-22 16:37:40 +0200

    show selected book details

--- app/src/main/java/com/example/bookkeeper/main_flow/book_details/BookDetailsFragment.kt
+++ app/src/main/java/com/example/bookkeeper/main_flow/book_details/BookDetailsFragment.kt
@@ -7,14 +7,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.bookkeeper.BookKeeperApplication
 import com.example.bookkeeper.R
 import com.example.bookkeeper.base_classes.BaseFragment
+import com.example.bookkeeper.database.entities.BooksEntity
 import com.example.bookkeeper.main_flow.books_search.BookSearchViewModel
 import com.example.bookkeeper.main_flow.books_search.BooksSearchFragment.Companion.SELECTED_BOOK_ID
+import com.example.bookkeeper.utils.loadUrlImage
+import kotlinx.android.synthetic.main.fragment_book_details.*
 
 class BookDetailsFragment : BaseFragment<BookDetailsViewModel>() {
 
+    private lateinit var navController: NavController
     private var bookDetailsViewMode = BookDetailsViewModel(BookKeeperApplication.getInstance())
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -23,12 +29,18 @@ class BookDetailsFragment : BaseFragment<BookDetailsViewModel>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
         initViewAndData()
     }
 
     private fun initViewAndData() {
         getIntentValues()
         getObservableForRequestedBook()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        backIcon.setOnClickListener { navController.popBackStack() }
     }
 
     private fun getIntentValues() {
@@ -40,6 +52,17 @@ class BookDetailsFragment : BaseFragment<BookDetailsViewModel>() {
 
     private fun getObservableForRequestedBook() {
         bookDetailsViewMode.selectedBook.observe(this.requireActivity(), Observer { selectedBook ->
+            setDataToViews(selectedBook)
         })
+    }
+
+    private fun setDataToViews(selectedBook: BooksEntity?) {
+        selectedBook?.let { book ->
+            book.volumeInfo.imageLinks?.smallThumbnail?.let { bookDetailsImage.loadUrlImage(it.replace("http", "https"), this.requireContext()) }
+            bookDetailsTitle.text = book.volumeInfo.title
+            bookDetailsSubtitle.text = book.volumeInfo.subtitle
+            bookRating.text = "4/5"
+            bookDescription.text = book.volumeInfo.description
+        }
     }
 }
