1296e05c70b97cae349ff257371528bc27fa25ee
evagoras <r1n1os.dev@gmail.com>
2021-06-08 22:23:38 +0300

    migrate liveData to flows

--- app/src/main/java/com/example/bookkeeper/main_flow/books_search/BookSearchViewModel.kt
+++ app/src/main/java/com/example/bookkeeper/main_flow/books_search/BookSearchViewModel.kt
@@ -9,18 +9,13 @@ import com.example.bookkeeper.database.entities.*
 import com.example.bookkeeper.database.entities.bojos.BooksAndBookDetailsBojo
 import com.example.bookkeeper.main_flow.books_search.model.SearchedBooksModel
 import com.example.bookkeeper.network.RetrofitService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 class BookSearchViewModel(application: Application) : BaseViewModel(application) {
 
     private var retrofitService = RetrofitService()
-    var booksResult = MutableLiveData<List<SearchedBooksModel>>()
+    var booksResult = MutableStateFlow<List<SearchedBooksModel>>(mutableListOf())
     /*var bookResult : LiveData<TestEntity> = liveData(Dispatchers.IO){
         val result = retrofitService.getRetrofitService().getSearchedBooks("Clean Code")
         emit(result)
@@ -35,24 +30,24 @@ class BookSearchViewModel(application: Application) : BaseViewModel(application)
     }
 
    private fun executeRequestForSearchedBook(searchKeyword: String) = flow{
-       var loadBooks = mutableListOf<SearchedBooksModel>()
-        withContext(Dispatchers.IO) {
+       //var loadBooks = mutableListOf<SearchedBooksModel>()
+       // withContext(Dispatchers.IO) {
             val requestResponse = retrofitService.getRetrofitService().getSearchedBooks(searchKeyword)
             if (requestResponse.isSuccessful) {
                 val bookResponse = requestResponse.body()
                 bookResponse?.books?.let { books ->
                     BooksEntity.insertBooks(books).collect {
-                        loadBooks = SearchedBooksModel.createBookSearchModel(it.toMutableList(), false)
+                     emit(SearchedBooksModel.createBookSearchModel(it.toMutableList(), false))
                     }
                 }
             } else {
-                errorResponse.value = requestResponse.message()
+                errorResponse.value = requestResponse.message() ?: "Something went wrong"
             }
-        }
-       emit(loadBooks)
-    }.catch {
-       Log.d("catchException", it.message)
-       errorResponse.value = it.message
+            //emit(loadBooks)
+       // }
+    }.flowOn(Dispatchers.Main)
+       .catch {
+           errorResponse.value = it.message ?: "Something went wrong"
    }
 
     fun getPreviousSearchedItemsFromDatabase(){
@@ -64,10 +59,11 @@ class BookSearchViewModel(application: Application) : BaseViewModel(application)
     }
 
     private fun executeLocalRequestToGetLastSearchedItems() = flow {
-        var books = mutableListOf<SearchedBooksModel>()
+        emit(SearchedBooksModel.createBookSearchModel(BookKeeperApplication.getInstance().getDatabaseInstance().booksDao().getAllBooks().toMutableList(), true))
+
+        /*var books = mutableListOf<SearchedBooksModel>()
         withContext(Dispatchers.IO){
-             books = SearchedBooksModel.createBookSearchModel(BookKeeperApplication.getInstance().getDatabaseInstance().booksDao().getAllBooks().toMutableList(), true)
         }
-        emit(books)
-    }
+        emit(books)*/
+    }.flowOn(Dispatchers.Main)
 }
