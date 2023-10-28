d5f8967be18e2aca84aad914e998cc1a8b8a8a70
r1n1os <evagoras.frang@gmail.com>
2023-10-28 23:55:57 +0300

    upgrade project to gradle 8.1.2, kotlin version 1.9.10, ksp and update all dependencies

--- app/src/main/java/com/example/bookkeeper/database/entities/BooksEntity.kt
+++ app/src/main/java/com/example/bookkeeper/database/entities/BooksEntity.kt
@@ -1,15 +1,12 @@
 package com.example.bookkeeper.database.entities
 
 import androidx.room.Entity
-import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.example.bookkeeper.BookKeeperApplication
 import com.example.bookkeeper.database.entities.type_converters.BookInfoTypeConverter
 import com.google.gson.annotations.SerializedName
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import org.jetbrains.annotations.Nullable
 
 @Entity
 data class BooksEntity(
